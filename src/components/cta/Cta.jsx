4d88c0a4af9bc98a4889ff3f301888a62f141831
Steven Abuan <stevenjosephabuan@gmail.com>
2022-09-02 21:39:30 +0800

    update resume

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -1,6 +1,6 @@
 import { Box, Button } from "@mui/material";
 import "animate.css";
-import cv from "../../assets/ABUANsteven-cv.pdf";
+import cv from "../../assets/ABUAN-CV.pdf";
 
 const Cta = () => {
   return (
